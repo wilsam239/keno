f078549a3ef2c621692f7bcc3114239d61e424a1
wilsam239 <swilliamson239@hotmail.com>
2022-08-24 21:07:02 +1000

    Expanded setup stuff

--- src/app/setup/setup.component.ts
+++ src/app/setup/setup.component.ts
@@ -1,19 +1,36 @@
 import { Component, OnDestroy, OnInit } from "@angular/core"
-import { NUMBER_OPTIONS } from "../commons/global"
+import { Router } from "@angular/router";
+import { AppRoutes, NUMBER_OPTIONS } from "../commons/global"
 
+interface SetupModel {
+    bet: number | undefined;
+    games: number | undefined;
+    numbers: number | undefined;
+    choices: number[]
+}
 @Component({
   selector: "app-setup",
   templateUrl: "./setup.component.html",
   styleUrls: ["./setup.component.scss"],
 })
 export class SetupComponent implements OnInit, OnDestroy {
-  title = "setup"
-  numberOptions = NUMBER_OPTIONS
-  numberOfNumbers?: number
+  title = "setup";
+  setupModel: SetupModel = {
+    bet: undefined,
+    games: undefined,
+    numbers: undefined,
+    choices: []
+  }
+  numberOptions = NUMBER_OPTIONS;
 
-  numGames = 1
+  constructor(private router: Router) {}
 
   ngOnInit() {}
 
   ngOnDestroy() {}
+
+  finalise() {
+    Object.entries(this.setupModel).filter(([k, v]) => v !== undefined).forEach(([k, v]) => window.localStorage.setItem(k, v));
+    this.router.navigate([AppRoutes.PLAY]);
+  }
 }
